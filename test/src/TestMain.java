356b6d272c73fbe13bb7c2fd2d228a107bb77c81
yinguoliang <yglah126@126.com>
2014-09-17 19:23:00 +0800

    modify TestMain.java

--- test/src/TestMain.java
+++ test/src/TestMain.java
@@ -7,24 +7,6 @@ import java.util.HashSet;
 
 public class TestMain {
 	public static void main(String rags[]) throws Exception{
-		File file = new File("f:/work/Program/CCIC/ProductSystemLog/xgsrv/out.log");
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		String line = null;
-		String packno = null;
-		HashMap<String,HashSet<String>> map = new HashMap<String,HashSet<String>>();
-		while((line=br.readLine())!=null){
-			if(line.startsWith("T")){
-				packno = line;
-			}
-			else if(line.startsWith("B")){
-				if(map.get(line)==null) map.put(line, new HashSet<String>());
-				map.get(line).add(packno);
-			}
-		}
-		br.close();
-		for(String key : map.keySet()){
-			if(map.get(key).size()>1)
-			   System.out.println(key+"   "+map.get(key));
-		}
+		
 	}
 }
